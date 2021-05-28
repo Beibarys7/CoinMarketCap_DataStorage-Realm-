609e835a65428f08d7368aec9cc95cba008b08ec
Beibarys <beibarys1927@gmail.com>
2021-05-28 21:48:52 +0600

    Initial commit

---
+++ Project7-DataStorage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7-DataStorage
+//
+//  Created by Бейбарыс Шагай on 12/8/20.
+//  Copyright © 2020 Бейбарыс Шагай. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
